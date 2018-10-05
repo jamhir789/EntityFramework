3598e66ca2093d23f86fe9b64e1cf0ec929913f1
jamhir789 <erick.medina@itspanuco.edu.mx>
2018-10-05 15:34:08 -0500

    inicial

---
+++ entityF-Ejemplo1/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace entityF
+{
+    class ApplicationDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+                                              //la ConnetionString de abaajo es para crear la base de datos desde entityframework
+        {                                  //"Data Source=EISEILAP0319;Initial Catalog=EntityF;Integrated Security=True"
+            optionsBuilder.UseSqlServer("Server=EISEILAP0319;Database=EntityF;Trusted_Connection=True;MultipleActiveResultSets=true")
+            .EnableSensitiveDataLogging(true)//esto nos sirver para que nos de un log y saber como actuan nuestras querys 
+            .UseLoggerFactory(new LoggerFactory().AddConsole((category, level) => level == LogLevel.Information && category == DbLoggerCategory.Database.Command.Name, true));
+        }
+
+        public DbSet<Estudiante> Estudiantes { get; set; }
+
+
+
+    }
+}
---
+++ entityF-Ejemplo1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace entityF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            //instanciamos nuestro dbcontext para cerrrar la conexion a bd
+            using (var context = new ApplicationDbContext()) 
+            {
+                var estudiante = new Estudiante();
+                estudiante.Nombre = "felipe";
+                context.Estudiantes.Add(estudiante);
+                context.SaveChanges();
+            }
+            Console.Write("LISTO");
+        }
+    }
+
+    class Estudiante {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+    }
+
+}
